e8bb1661b517976255355120590301d834b9b486
BlackIzy <104522485+BlackIzy@users.noreply.github.com>
2023-03-10 15:44:03 +0100

    Delete src/main/java/apirest directory

--- src/main/java/apirest/repositories/DogRepository.java
+++
@@ -1,9 +0,0 @@
-package apirest.repositories;
-
-import apirest.entities.Dog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DogRepository extends JpaRepository<Dog, Long> {
-}
